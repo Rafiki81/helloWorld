46947c03f45f8991a5723b9cc83f8787adbcf9f0
rafael <rperezbeato@gmail.com>
2019-12-08 23:31:54 +0100

    Correcciones

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,7 +3,7 @@ import org.apache.commons.lang3.StringUtils;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.printf("HelloWorld");
-        System.out.printf(StringUtils.capitalize("hello world");
+        System.out.println("HelloWorld");
+        System.out.println(StringUtils.capitalize("hello world"));
     }
 }
