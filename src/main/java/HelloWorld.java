0c80af60f70a8571ca73e1a7e3139275252b9fcd
Rafiki81 <rperezbeato@gmail.com>
2019-12-08 12:13:43 +0100

    Añadida dependencia en POM

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,9 @@
+import org.apache.commons.lang3.StringUtils;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.printf("HelloWorld");
+        System.out.printf(StringUtils.capitalize("hello world");
     }
 }
